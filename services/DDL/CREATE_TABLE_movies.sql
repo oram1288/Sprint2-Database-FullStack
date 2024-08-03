652dcd93d3d58f6b9253851e3f4fac1c009347e2
oram1288 <cody.oram@keyin.com>
2024-08-02 21:46:59 -0230

    Postgres and mongodb js files created, with logEvents and index file created as well

--- services/DDL/CREATE_TABLE_movies.sql
+++ services/DDL/CREATE_TABLE_movies.sql
@@ -3,7 +3,7 @@
 CREATE TABLE movies (
     id SERIAL PRIMARY KEY NOT NULL,
     movie_name VARCHAR(255) NOT NULL,
-    movie_genre VARCHAR(50),
+    movie_genre VARCHAR(50) NOT NULL,
     movie_runtime INTEGER NOT NULL,
     release_date DATE NOT NULL,
     director VARCHAR(100) NOT NULL
---
+++ services/p.fulltext.dal.js
@@ -0,0 +1,32 @@
+const dal = require("./p.db");
+
+var getFullText = function (text) {
+  if (DEBUG) console.log("postgres.dal.getFullText()");
+  return new Promise(function (resolve, reject) {
+    const sql = `SELECT movie_name, movie_genre, movie_runtime, director FROM movies \
+    WHERE director iLIKE '%'||$1||'%' \
+        OR movie_name iLIKE '%'||$1||'%' \
+        OR movie_genre iLIKE '%'||$1||'%'\
+        OR movie_runtime iLIKE '%'||$1||'%'`;
+
+    //  const sql = `SELECT movie_name, director FROM movies \
+    //     WHERE director iLIKE '%'||$1||'%' \
+    //     OR movie_name iLIKE '%'||$1||'%'`;
+
+    if (DEBUG) console.log(sql);
+    dal.query(sql, [text], (err, result) => {
+      if (err) {
+        // logging should go here
+        if (DEBUG) console.log(err);
+        reject(err);
+      } else {
+        if (DEBUG) console.log(`Row count: ${result.rowCount}`);
+        resolve(result.rows);
+      }
+    });
+  });
+};
+
+module.exports = {
+  getFullText,
+};
