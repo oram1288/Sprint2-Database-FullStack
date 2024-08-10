32e9636a8dae6902148c469b9c1a4c76a6b7aa53
oram1288 <cody.oram@keyin.com>
2024-08-10 17:19:13 -0230

    Some fixes and testing the code

--- routes/search.js
+++ routes/search.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { setToken, authenticateJWT } = require('../services/auth');
-const myEventEmitter = require('../services/logEvents.js');
+const { setToken, authenticateJWT } = require("../services/auth");
+const myEventEmitter = require("../services/logEvents.js");
 
-const pDal = require('../services/p.fulltext.dal');
-const mDal = require('../services/m.fulltext.dal');
+const pDal = require("../services/p.fulltext.dal");
+// const mDal = require("../services/m.fulltext.dal");
 
 // Use the setToken middleware to set the JWT token from the session
 router.use(setToken);
@@ -12,24 +12,35 @@ router.use(setToken);
 // Protect all API routes with the authenticateJWT middleware
 router.use(authenticateJWT);
 
-router.get('/', (req, res) => {
-    const theResults = [];
-    myEventEmitter.emit('event', 'app.get /search', 'INFO', 'search page (search.ejs) was displayed.');
-    res.render('search', { status: req.session.status, theResults });
+router.get("/", (req, res) => {
+  const theResults = [];
+  myEventEmitter.emit(
+    "event",
+    "app.get /search",
+    "INFO",
+    "search page (search.ejs) was displayed."
+  );
+  res.render("search", { status: req.session.status, theResults });
 });
 
-router.post('/', async (req, res) => {
-    let theResults;
-    try {
-        // Replace with the desired DAL based on the database choice
-        theResults = await mDal.getFullText(req.body.keyword);
-        myEventEmitter.emit('event', 'app.post /search', 'INFO', 'search page (search.ejs) was displayed.');
-        res.render('search', { status: req.session.status, theResults });
-    } catch (error) {
-        console.error(error);
-        req.session.status = 'Search failed.';
-        res.render('search', { status: req.session.status, theResults: [] });
-    }
+router.post("/", async (req, res) => {
+  let theResults;
+  try {
+    // Replace with the desired DAL based on the database choice
+    theResults = await pDal.getFullText(req.body.keyword);
+    // theResults = await mDal.getFullText(req.body.keyword);
+    myEventEmitter.emit(
+      "event",
+      "app.post /search",
+      "INFO",
+      "search page (search.ejs) was displayed."
+    );
+    res.render("search", { status: req.session.status, theResults });
+  } catch (error) {
+    console.error(error);
+    req.session.status = "Search failed.";
+    res.render("search", { status: req.session.status, theResults: [] });
+  }
 });
 
 module.exports = router;
